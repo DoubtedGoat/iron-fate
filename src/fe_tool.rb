4042aacdb3c5c15c6b77eac2900320aad3c89cd0
Jeremie Clark <clark.jeremie@gmail.com>
2016-03-24 17:33:34 -0400

    Abstract DB adapter for fe_tool.rb

--- src/fe_tool.rb
+++ src/fe_tool.rb
@@ -1,28 +1,32 @@
 require 'sinatra'
 require 'sqlite3'
+require 'pg'
 #require 'jbuilder'
 require 'json'
 
-class FEService < Sinatra::Base
 
+class FEService < Sinatra::Base
   get '/' do
     "Hello World"
   end
   
   get '/characters/all' do 
     content_type :json
-    db = SQLite3::Database.open './db/fe.db'
-    res = db.execute 'select shortname from characters'
+    db = PGAdapter.new
+    db.connect('fedb')
+    res = db.execute('select shortname from characters')
     res = res.flatten
     res.to_json
+    db.close
   end
 
   get '/characters/:name' do
+    db = PGAdapter.new
+    db.connect('fedb')
     content_type :json
-    db = SQLite3::Database.open './db/fe.db'
-    db.results_as_hash = true
-    res = db.execute 'select * from characters where shortname like ?', params['name']
+    res = db.execute("select * from characters where shortname like '#{params['name']}'")
     res.to_json
+    db.close
   end
 
 end
