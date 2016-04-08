f1735ae5f2b5bc4ca2f55b0a5df2379d90fd99ea
ddemons <ddemons@users.noreply.github.com>
2016-04-08 01:11:05 -0400

    Some weird experiment that wont work

--- public/app.js
+++ public/app.js
@@ -3,11 +3,17 @@ var app = angular.module('myApp', []);
 //add a controller to it
 app.controller('MyCtrl', function($scope, $http) {
 
+$scope.display = false;
    //a scope function to load the data.
    $scope.loadData = function () {
       $http.get('/characters/all').success(function(data) {
          $scope.items = data;
       });
    };
+   
+   $scope.unitClicked = function () {
+	    $scope.display = $scope.!display;
+   };
+   
 $scope.loadData();
 });
