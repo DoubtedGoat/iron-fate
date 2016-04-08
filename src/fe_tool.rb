8b4110cd30fd3f86b29dc8114424e7275044d012
Jeremie Clark <clark.jeremie@gmail.com>
2016-04-07 22:24:35 -0400

    Added class information route

--- src/fe_tool.rb
+++ src/fe_tool.rb
@@ -34,4 +34,20 @@ class FEService < Sinatra::Base
     res.to_json
   end
 
+  get '/classes/:name' do
+    db = PGAdapter.new
+    db.connect('fedb')
+    content_type :json
+    class_data = db.execute("select * from classes where shortname = '#{params['name']}'")
+    res = class_data.first
+    weapons = db.execute("select weapons.shortname, weapons.name from classes join classes_to_weapons as c2w on classes.shortname=c2w.class join weapons on c2w.weapon=weapons.shortname where classes.shortname='#{params['name']}'")
+    res['weapons'] = weapons
+
+    skills = db.execute("select skills.shortname, skills.name from classes join classes_to_skills as c2s on classes.shortname=c2s.class join skills on c2s.weapon=skills.shortname where classes.shortname='#{params['name']}'")
+    res['skills'] = skills
+
+    db.close
+    res.to_json
+  end
+
 end
