43c0e2844a701001cc977d106d360498f84ab09e
ddemons <ddemons@users.noreply.github.com>
2016-04-12 12:55:37 -0400

    putting it back

--- public/app.js
+++ public/app.js
@@ -17,6 +17,34 @@ $scope.display = false;
 $scope.loadData();
 });
 
+app.directive('unitBigInfo', [function() {
+	return {
+	controller: unitBigIController,
+	restrict: 'E',
+	scope: {
+	    shortname: '='
+	},
+	templateUrl: 'unitBigInfo.html'
+    };
+	
+	
+    var unitBigIController = ['$scope', function($scope){
+	$scope.unitClicked = function () {
+	    $scope.display = !$scope.display;
+	};
+	
+	$scope.loadMoreData = function (shortname) {
+	    /*$http.get(('/characters/'+ shortname)).success(function(data) {
+		$scope.moreData = data;
+	    });*/
+	};
+	
+	$scope.loadMoreData();
+    }];
+
+
+    
+}]);
 
 
 
