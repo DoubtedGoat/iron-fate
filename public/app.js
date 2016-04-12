cc24965882f6b3930703b7516a7aca80e636f415
ddemons <ddemons@users.noreply.github.com>
2016-04-12 16:02:07 -0400

    playing with css

--- public/app.js
+++ public/app.js
@@ -49,6 +49,17 @@ app.directive('unitBigInfo', [function() {
 }]);
 
 
+app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')',
+            'background-size' : 'cover'
+        });
+    };
+});
+
+
 
 
 
